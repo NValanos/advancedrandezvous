9064802ba64ee66ef50e25bdbff38143cb52bf0f
jimmysk <jimarkos@gmail.com>
2017-09-08 22:14:02 +0300

    Some Firsts Layouts

---
+++ app/src/main/java/com/unipi/mpsp160_02_12/advancedrandezvous/CreateEvent.java
@@ -0,0 +1,13 @@
+package com.unipi.mpsp160_02_12.advancedrandezvous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateEvent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+    }
+}
---
+++ app/src/main/java/com/unipi/mpsp160_02_12/advancedrandezvous/MainActivity.java
@@ -0,0 +1,13 @@
+package com.unipi.mpsp160_02_12.advancedrandezvous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
