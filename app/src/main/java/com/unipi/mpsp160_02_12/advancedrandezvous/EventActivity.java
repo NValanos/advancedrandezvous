4df5e45ebdcf68d03433ed9ffc97b8209ea5030c
NValanos <nvvalanos@gmail.com>
2017-10-06 01:18:57 +0300

    Check in created except saving to db

--- app/src/main/java/com/unipi/mpsp160_02_12/advancedrandezvous/EventActivity.java
+++ app/src/main/java/com/unipi/mpsp160_02_12/advancedrandezvous/EventActivity.java
@@ -1,9 +1,14 @@
 package com.unipi.mpsp160_02_12.advancedrandezvous;
 
 import android.app.Activity;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
@@ -29,6 +34,7 @@ import com.google.firebase.database.ValueEventListener;
 import com.unipi.mpsp160_02_12.advancedrandezvous.models.Event;
 import com.unipi.mpsp160_02_12.advancedrandezvous.models.LatLong;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import static android.content.ContentValues.TAG;
@@ -45,14 +51,25 @@ public class EventActivity extends Activity implements OnMapReadyCallback, Adapt
     private LatLong latLong;
     private LatLng mapsLatLng = new LatLng(0,0);
     private Button managePartitipantsButton;
+    private Button checkInButton;
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_activity);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        locationManager=(LocationManager)getSystemService(LOCATION_SERVICE);
         titleTextView = (TextView)findViewById(R.id.eventName);
         dateTextView = (TextView)findViewById(R.id.eventDate);
+        checkInButton = (Button)findViewById(R.id.checkInButton);
+
+        checkInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkCurrentLocation();
+            }
+        });
 
         managePartitipantsButton = (Button)findViewById(R.id.managePartitipantsButton);
         managePartitipantsButton.setOnClickListener(new View.OnClickListener() {
@@ -131,6 +148,12 @@ public class EventActivity extends Activity implements OnMapReadyCallback, Adapt
     @Override
     public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
         String stateSelected = parent.getItemAtPosition(position).toString();
+        if ("Accept".equals(stateSelected)){
+            checkInButton.setVisibility(View.VISIBLE);
+        }
+        else{
+            checkInButton.setVisibility(View.INVISIBLE);
+        }
         Toast.makeText(this, stateSelected, Toast.LENGTH_SHORT).show();
     }
 
@@ -138,4 +161,68 @@ public class EventActivity extends Activity implements OnMapReadyCallback, Adapt
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    private void checkCurrentLocation(){
+
+        if (ActivityCompat.checkSelfPermission(EventActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (location != null) {
+
+                float distance[] = new float [3];
+
+                Location.distanceBetween(location.getLatitude(), location.getLongitude(), event.getLocation().getLatitude(), event.getLocation().getLongitude(), distance);
+                if (distance[0] <= 100){
+                    Date eventDate = new Date(event.getDate());
+                    Date currentTime = Calendar.getInstance().getTime();
+                    Calendar afterEventDate = Calendar.getInstance();
+                    afterEventDate.setTime(eventDate);
+                    afterEventDate.add(Calendar.HOUR_OF_DAY, 1);
+                    System.out.println(currentTime.toString());
+                    System.out.println(eventDate.toString());
+                    if (currentTime.after(eventDate) && currentTime.before(afterEventDate.getTime())){
+                        Toast.makeText(this, "Welcome to the event", Toast.LENGTH_SHORT).show();
+                    }
+                    else if (currentTime.after(eventDate)){
+                        Toast.makeText(this, "The event date has passed", Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Toast.makeText(this, "It is not time for the event yet", Toast.LENGTH_SHORT).show();
+                    }
+                }
+                else{
+                    Toast.makeText(this, "Please get close to the event to check in", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        }
+        else{
+            Toast.makeText(EventActivity.this, "Cannot establish location. GPS usage permission is denied", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    private class MyLocationListener implements LocationListener {
+        public void onLocationChanged(Location location) {
+            String message = String.format(
+                    "New Location \n Longitude: %1$s \n Latitude: %2$s",
+                    location.getLongitude(), location.getLatitude()
+            );
+            Toast.makeText(EventActivity.this, message, Toast.LENGTH_LONG).show();
+        }
+        public void onStatusChanged(String s, int i, Bundle b) {
+            Toast.makeText(EventActivity.this, "Provider status changed",
+                    Toast.LENGTH_LONG).show();
+        }
+        public void onProviderDisabled(String s) {
+            Toast.makeText(EventActivity.this,
+                    "Provider disabled by the user. GPS turned off",
+                    Toast.LENGTH_LONG).show();
+        }
+        public void onProviderEnabled(String s) {
+            Toast.makeText(EventActivity.this,
+                    "Provider enabled by the user. GPS turned on",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
